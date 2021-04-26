6335a941088f95190bcf8eb35f4d8b6cf17484b4
Josh Oettinger <josh@Joshs-MBP.attlocal.net>
2021-04-25 19:20:41 -0500

    Added react site

--- hardhat.config.js
+++ hardhat.config.js
@@ -10,13 +10,26 @@ task("accounts", "Prints the list of accounts", async () => {
   }
 });
 
-// You need to export an object to set up your config
-// Go to https://hardhat.org/config/ to learn more
+// Go to https://www.alchemyapi.io, sign up, create
+// a new App in its dashboard, and replace "KEY" with its key
+const ALCHEMY_API_KEY = "GDfBZqo1CjIB8CMdHlqg8hKJWwSqy9Nu";
+
+// Replace this private key with your Ropsten account private key
+// To export your private key from Metamask, open Metamask and
+// go to Account Details > Export Private Key
+// Be aware of NEVER putting real Ether into testing accounts
+const ROPSTEN_PRIVATE_KEY = "3a36ea6587bc3872c00d51cbe85507b350f807cac62db1cc8bd9cf8c4b558a69";
 
 /**
  * @type import('hardhat/config').HardhatUserConfig
  */
 module.exports = {
   solidity: "0.8.4",
+  networks: {
+    ropsten: {
+      url: `https://eth-ropsten.alchemyapi.io/v2/${ALCHEMY_API_KEY}`,
+      accounts: [`0x${ROPSTEN_PRIVATE_KEY}`]
+    }
+  }
 };
 
