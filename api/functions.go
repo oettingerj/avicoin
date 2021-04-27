13a852c70757308a49d029acce3d7f7ffa810ada
Josh Oettinger <josh@Joshs-MBP.attlocal.net>
2021-04-26 22:33:07 -0500

    initialized go

---
+++ api/functions.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	"fmt"
+	"github.com/ethereum/go-ethereum/ethclient"
+	"firebase.google.com/go/v4"
+	"log"
+)
+
+func main() {
+	client, err := ethclient.Dial("https://eth-ropsten.alchemyapi.io/v2/GDfBZqo1CjIB8CMdHlqg8hKJWwSqy9Nu")
+}
+
---
+++ api/go.mod
@@ -0,0 +1,8 @@
+module api
+
+go 1.16
+
+require (
+	firebase.google.com/go/v4 v4.5.0
+	github.com/ethereum/go-ethereum v1.10.2
+)
